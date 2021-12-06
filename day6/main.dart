fa68c19b67a67375c53cd3ce62c95a8237a4f2cf
matyjb <matyjasiakbartosz@gmail.com>
2021-12-06 16:50:50 +0100

    using list, not map

--- day6/main.dart
+++ day6/main.dart
@@ -4,19 +4,21 @@
 
 import 'dart:io';
 
-void simulationStep(Map<int, int> oceanState) {
+void simulationStep(List<int> oceanState) {
   int createdFishesThisDay = 0;
   for (var j = 0; j <= 8; j++) {
-    int currentFishesTimerState = oceanState[j] ?? 0;
-    oceanState.update(j, (value) => 0);
+    int currentFishesTimerState = oceanState[j];
+    // just a shift down the array [j-1] <- [j]
+    oceanState[j] = 0;
     if (j == 0) {
       createdFishesThisDay += currentFishesTimerState;
     } else {
-      oceanState.update(j - 1, (value) => value + currentFishesTimerState);
+      oceanState[j - 1] += currentFishesTimerState;
     }
   }
-  oceanState.update(8, (value) => createdFishesThisDay);
-  oceanState.update(6, (value) => value + createdFishesThisDay);
+  // special cases when needed to update upper values after shift down happens
+  oceanState[8] = createdFishesThisDay;
+  oceanState[6] += createdFishesThisDay;
 }
 
 void main(List<String> args) {
@@ -29,21 +31,14 @@ void main(List<String> args) {
   int maxDays = 256;
 
   //map of fish timer to how many of these fishes there are with that timer state
-  Map<int, int> oceanState = {
-    for (var i = 0; i < 9; i++) i: 0,
-  };
-  initialFishes.forEach(
-    (fish) => oceanState.update(
-      fish,
-      (value) => value + 1,
-    ),
-  );
+  List<int> oceanState = List.filled(9, 0);
+  initialFishes.forEach((fishTimer) => oceanState[fishTimer] += 1);
 
   // solution
   for (var i = 1; i <= maxDays; i++) {
     simulationStep(oceanState);
     if (i == 80 || i == 256) {
-      int sumOfAllFishes = oceanState.values.fold<int>(
+      int sumOfAllFishes = oceanState.fold<int>(
         0,
         (previousValue, element) => previousValue + element,
       );
