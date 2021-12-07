20e1164b5241dc0e08898fddd76444d9f104149e
matyjb <matyjasiakbartosz@gmail.com>
2021-12-07 15:15:44 +0100

    small optimization with map

--- day7/main.dart
+++ day7/main.dart
@@ -20,24 +20,50 @@ void main(List<String> args) {
               crabsHeights[(crabsHeights.length ~/ 2)]) /
           2
       : crabsHeights[(crabsHeights.length - 1) ~/ 2];
-  int fuelCost = crabsHeights.fold<int>(0, (fuelCostSum, element) => fuelCostSum + (element - median.toInt()).abs());
-  print(fuelCost);
+  int fuelCost = crabsHeights.fold<int>(0,
+      (fuelCostSum, element) => fuelCostSum + (element - median.toInt()).abs());
+  print("Final target position: ${median.toInt()}");
+  print("Final fuel cost:       $fuelCost");
 
   print("## Part 2 ##");
   // super ultra naive and extremly bad solution
+
+  // helper functions
   int calcFuelCostForPosToPos(int pos0, int pos1) {
     int sum = 0;
-    for (var i = 0; i < (pos0-pos1).abs(); i++) {
-      sum += i+1;
+    for (var i = 0; i < (pos0 - pos1).abs(); i++) {
+      sum += i + 1;
     }
     return sum;
   }
 
-  int minFuelCostCandidate = crabsHeights.fold(0, (fuelCostSum, element) => fuelCostSum + calcFuelCostForPosToPos(element, crabsHeights[0]));
+  int calcSubFuelCost(int fuelCostSum, MapEntry<int, int> element, int pos) =>
+      fuelCostSum + calcFuelCostForPosToPos(element.key, pos) * element.value;
+  ///
+    
+  Map<int, int> crabsHeightsDict = Map<int, int>();
+  crabsHeights.forEach((element) {
+    crabsHeightsDict.update(element, (value) => value + 1, ifAbsent: () => 1);
+  });
+
+  // initial candidate
+  int minFuelCost = crabsHeightsDict.entries.fold(
+    0,
+    (fuelCostSum, element) =>
+        calcSubFuelCost(fuelCostSum, element, crabsHeights[0]),
+  );
+  int finalTarget = 0;
+  // searching for the better candidate
   for (var i = crabsHeights[1]; i <= crabsHeights.last; i++) {
-    int candidate = crabsHeights.fold(0, (fuelCostSum, element) => fuelCostSum + calcFuelCostForPosToPos(element, i));
-    if(candidate < minFuelCostCandidate)
-      minFuelCostCandidate = candidate;
+    int candidate = crabsHeightsDict.entries.fold(
+      0,
+      (fuelCostSum, element) => calcSubFuelCost(fuelCostSum, element, i),
+    );
+    if (candidate < minFuelCost) {
+      minFuelCost = candidate;
+      finalTarget = i;
+    }
   }
-  print(minFuelCostCandidate);
+  print("Final target position: $finalTarget");
+  print("Final fuel cost:       $minFuelCost");
 }
