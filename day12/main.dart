8d6cd1ee44d9d3a01d4913cb830220bc1e98a516
matyjb <matyjasiakbartosz@gmail.com>
2021-12-12 20:12:18 +0100

    day12 part2

--- day12/main.dart
+++ day12/main.dart
@@ -7,30 +7,75 @@ import 'dart:io';
 bool isBigCave(String s) => s.toUpperCase() == s && s != "start" && s != "end";
 Iterable<List<String>> dfsFindAllPathsPart1(
   Map<String, List<String>> graph,
-  List<String> currentPathStack,
-  Set<String> visitedSmallCaves, [
+  List<String> currentPathStack, [
   String start = "start",
   String end = "end",
 ]) sync* {
   if (start == end) {
     yield currentPathStack + [start];
-  }
+  } else {
+    currentPathStack.add(start);
+    List<String> possibleNextCaves = graph[start]!
+        .where((element) =>
+            isBigCave(element) || !currentPathStack.contains(element))
+        .toList();
 
-  currentPathStack.add(start);
-  if (!isBigCave(start)) visitedSmallCaves.add(start);
-  List<String> possibleNextCaves = graph[start]!
-      .where((element) => !visitedSmallCaves.contains(element))
-      .toList();
-
-  for (var nextCave in possibleNextCaves) {
-    for (var path in dfsFindAllPathsPart1(
-        graph, currentPathStack, visitedSmallCaves, nextCave)) {
-      yield path;
+    for (var nextCave in possibleNextCaves) {
+      for (var path
+          in dfsFindAllPathsPart1(graph, currentPathStack, nextCave)) {
+        yield path;
+      }
     }
-  }
 
-  visitedSmallCaves.remove(start);
-  currentPathStack.removeLast();
+    currentPathStack.removeLast();
+  }
+}
+
+Iterable<List<String>> dfsFindAllPathsPart2(
+  Map<String, List<String>> graph,
+  List<String> currentPathStack, [
+  bool hasDuplicateInPathStack = false,
+  String start = "start",
+  String end = "end",
+]) sync* {
+  if (start == end) {
+    yield currentPathStack + [start];
+  } else {
+    currentPathStack.add(start);
+    List<String> possibleNextCaves;
+
+    if (hasDuplicateInPathStack)
+      possibleNextCaves = graph[start]!
+          .where(
+            (element) =>
+                isBigCave(element) || !currentPathStack.contains(element),
+          )
+          .toList();
+    else
+      possibleNextCaves = graph[start]!
+          .where(
+            (element) => element != "start",
+          )
+          .toList();
+
+    for (var nextCave in possibleNextCaves) {
+      bool hasDup =
+          currentPathStack.contains(nextCave) && !isBigCave(nextCave) ||
+              hasDuplicateInPathStack;
+      var paths = dfsFindAllPathsPart2(
+        graph,
+        currentPathStack,
+        hasDup,
+        nextCave,
+      );
+      for (var path in paths) {
+        yield path;
+      }
+      
+    }
+
+    currentPathStack.removeLast();
+  }
 }
 
 void main(List<String> args) {
@@ -52,12 +97,15 @@ void main(List<String> args) {
   int keysMaxWidth = graph.keys.fold(
       0, (value, element) => element.length > value ? element.length : value);
   graph.forEach((key, value) =>
-      print("\x1B[32m${key.padLeft(keysMaxWidth)}\x1B[0m: $value"));
+      print("\x1B[32m${key.padLeft(keysMaxWidth)}\x1B[0m => $value"));
 
   print("\x1B[32m## Part 1 ##\x1B[0m");
-  int pathsCount = dfsFindAllPathsPart1(graph, [], {})
+  int pathsCount = dfsFindAllPathsPart1(graph, [])
       .fold<int>(0, (count, element) => count + 1);
   print("Found \x1B[33m$pathsCount\x1B[0m possible paths.");
+
   print("\x1B[32m## Part 2 ##\x1B[0m");
-  
+  int pathsCount2 = dfsFindAllPathsPart2(graph, [])
+      .fold<int>(0, (count, element) => count + 1);
+  print("Found \x1B[33m$pathsCount2\x1B[0m possible paths.");
 }
